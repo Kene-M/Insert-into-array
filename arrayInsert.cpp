deb8a409b27ce54021df566336a47a87de287e93
Kene-M <109693458+Kene-M@users.noreply.github.com>
2022-07-28 16:27:29 -0400

    Add files via upload

---
+++ arrayInsert.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+
+using namespace std;
+
+int findId(int, int [], int);
+int insertId(int, int [], int);
+
+int main()
+{
+    const int MAX_SIZE = 100;
+
+    int sid[MAX_SIZE] = { 1, 3, 6, 8, 10};
+    int index, id, sz = 5;
+
+        cout << "Enter the student id: ";
+        cin >> id;
+        index = findId(id, sid, sz);
+        if (index > 0){
+            cout << id << " found at position " << index << endl;
+        }
+        else{
+            cout << id << " not found \n";
+        }
+    insertId(id, sid, sz);
+
+
+
+    for (int i=0; i < sz+1; i++)
+        cout << sid[i] << " ";
+
+
+}
+
+// should return the index of val if it is present in the array
+// otherwise return -1
+int findId(int val, int arrId[], int sz)
+{
+    for (int i = 0; i < sz; i++){
+            if (val == arrId[i])
+                return i;
+    }
+    return(-1);
+}
+
+int insertId(int val, int arrId[], int sz)
+{
+    int i;
+    for (i = 0; i < sz; i++){
+            if (val == arrId[i]){
+                cout << val << " already present " << endl;
+                return i;
+            }
+            else if (val < arrId[i])
+                break; // found the position to insert
+    }
+    // position found. Now move everyone to the right by one position
+    for (int j = sz - 1; j >= i; j--){
+         arrId[j+1] = arrId[j]; // move one position to the right
+    }
+    arrId[i] = val;
+    cout << val << " inserted into the array" << endl;
+}
